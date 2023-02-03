66b8888277be92fb97e8a6937e32e870ecdc90d6
Asterecho <2548299040@qq.com>
2023-02-03 19:03:10 +0800

    +

---
+++ WPE/MainForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： Administrator
+ * 日期: 2021/12/13/周一
+ * 时间: 2:33
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+namespace WPE
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.donateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.button1 = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.contextMenuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1MouseDoubleClick);
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.donateToolStripMenuItem,
+			this.aboutToolStripMenuItem,
+			this.exitToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(101, 70);
+			// 
+			// donateToolStripMenuItem
+			// 
+			this.donateToolStripMenuItem.Name = "donateToolStripMenuItem";
+			this.donateToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+			this.donateToolStripMenuItem.Text = "捐助";
+			this.donateToolStripMenuItem.Click += new System.EventHandler(this.DonateToolStripMenuItemClick);
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+			this.aboutToolStripMenuItem.Text = "关于";
+			this.aboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItemClick);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+			this.exitToolStripMenuItem.Text = "退出";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItemClick);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(38, 24);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(131, 58);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "设置";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(208, 236);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "WPE";
+			this.TopMost = true;
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFormFormClosing);
+			this.Resize += new System.EventHandler(this.MainFormResize);
+			this.contextMenuStrip1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.ToolStripMenuItem donateToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+	}
+}
---
+++ WPE/MainForm.cs
@@ -0,0 +1,234 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： Administrator
+ * 日期: 2021/12/13/周一
+ * 时间: 2:33
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WPE
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		// 指向 Program Manager 窗口句柄
+	    private IntPtr programIntPtr = IntPtr.Zero;
+	
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			Process.Start("ffplay.exe ", " *.mp4 -an  -loop 0  -fs");
+			Thread.Sleep(1000);
+			this.play();
+			base.WindowState = FormWindowState.Minimized;
+			flag=true;
+			this.notifyIcon1.Visible = true;
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		private void play()
+		{
+			this.Init();
+			IntPtr hwnd = Win32.FindWindow("SDL_app", null);
+			IntPtr hwnd2 = Win32.FindWindow("ConsoleWindowClass", null);
+			Win32.ShowWindow(hwnd2, 0);
+			Win32.SetParent(hwnd, this.programIntPtr);
+		}
+		public void Init()
+	    {
+	        // 通过类名查找一个窗口，返回窗口句柄。
+	        programIntPtr = Win32.FindWindow("Progman", null);
+	
+	        // 窗口句柄有效
+	        if(programIntPtr != IntPtr.Zero)
+	        {   
+	
+	            IntPtr result = IntPtr.Zero;
+	
+	            // 向 Program Manager 窗口发送 0x52c 的一个消息，超时设置为0x3e8（1秒）。
+	            Win32.SendMessageTimeout(programIntPtr, 0x52c, IntPtr.Zero, IntPtr.Zero, 0, 0x3e8, result);
+	
+	            // 遍历顶级窗口
+	            Win32.EnumWindows((hwnd, lParam) =>
+	            {
+	                // 找到包含 SHELLDLL_DefView 这个窗口句柄的 WorkerW
+	                if (Win32.FindWindowEx(hwnd,IntPtr.Zero, "SHELLDLL_DefView", null) != IntPtr.Zero)
+	                {
+	                    // 找到当前 WorkerW 窗口的，后一个 WorkerW 窗口。 
+	                    IntPtr tempHwnd = Win32.FindWindowEx(IntPtr.Zero, hwnd, "WorkerW", null);
+	
+	                    // 隐藏这个窗口
+	                    Win32.ShowWindow(tempHwnd, 0);
+	                }
+	                return true;
+	            }, IntPtr.Zero);
+	        }
+	    }
+		
+	 
+		const int WM_CLOSE = 0x0010;
+		void ExitToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			//找到控制台窗口
+	        IntPtr cmdIntPtr = Win32.FindWindow("ConsoleWindowClass", null);
+			Win32.SendMessage(cmdIntPtr, WM_CLOSE, 0, 0);
+		 
+			this.Close();
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+				// 初始化桌面窗口
+	            Init();
+	            //找到播放器窗口    
+	            IntPtr ffplayIntPtr = Win32.FindWindow("SDL_app", null);
+	            //找到控制台窗口
+	            IntPtr cmdIntPtr = Win32.FindWindow("ConsoleWindowClass", null);
+	            //隐藏控制台窗口
+	            Win32.ShowWindow(cmdIntPtr, 0);
+	            	
+	            // 窗口置父，设置背景窗口的父窗口为 Program Manager 窗口
+	            Win32.SetParent(ffplayIntPtr, programIntPtr);
+	            
+	            
+	            
+		}
+		
+		void MainFormResize(object sender, EventArgs e)
+		{
+			if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Visible=false;
+                this.notifyIcon1.Visible = true;
+			}else{
+				
+                this.notifyIcon1.Visible = false;
+			}
+		}
+		
+		void NotifyIcon1MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+//			this.Visible=true;
+//			this.WindowState =FormWindowState.Normal;
+		}
+		
+		 
+		
+		void MainFormFormClosing(object sender, FormClosingEventArgs e)
+		{
+			//找到控制台窗口
+	        IntPtr cmdIntPtr = Win32.FindWindow("ConsoleWindowClass", null);
+			Win32.SendMessage(cmdIntPtr, WM_CLOSE, 0, 0);
+		 
+			 
+		}
+		
+		/// <summary>
+		/// 转换Image为Icon
+		/// </summary>
+		/// <param name="image">要转换为图标的Image对象</param>
+		/// <param name="nullTonull">当image为null时是否返回null。false则抛空引用异常</param>
+		/// <exception cref="ArgumentNullException" />
+		public static Icon ConvertToIcon(Image image, bool nullTonull = false)
+		{
+		    if (image == null)
+		    {
+		        if (nullTonull) { return null; }
+		        throw new ArgumentNullException("image");
+		    }
+		
+		    using (MemoryStream msImg = new MemoryStream()
+		                      , msIco = new MemoryStream())
+		    {
+		        image.Save(msImg, ImageFormat.Png);
+		
+		        using (var bin = new BinaryWriter(msIco))
+		        {
+		            //写图标头部
+		            bin.Write((short)0);           //0-1保留
+		            bin.Write((short)1);           //2-3文件类型。1=图标, 2=光标
+		            bin.Write((short)1);           //4-5图像数量（图标可以包含多个图像）
+		
+		            bin.Write((byte)image.Width);  //6图标宽度
+		            bin.Write((byte)image.Height); //7图标高度
+		            bin.Write((byte)0);            //8颜色数（若像素位深>=8，填0。这是显然的，达到8bpp的颜色数最少是256，byte不够表示）
+		            bin.Write((byte)0);            //9保留。必须为0
+		            bin.Write((short)0);           //10-11调色板
+		            bin.Write((short)32);          //12-13位深
+		            bin.Write((int)msImg.Length);  //14-17位图数据大小
+		            bin.Write(22);                 //18-21位图数据起始字节
+		
+		            //写图像数据
+		            bin.Write(msImg.ToArray());
+		
+		            bin.Flush();
+		            bin.Seek(0, SeekOrigin.Begin);
+		            return new Icon(msIco);
+		        }
+		    }
+		}
+		public static Bitmap rotateImage(Bitmap b, float angle)
+{
+    //create a new empty bitmap to hold rotated image
+    Bitmap returnBitmap = new Bitmap(b.Width, b.Height);
+    //make a graphics object from the empty bitmap
+    Graphics g = Graphics.FromImage(returnBitmap);
+    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+    //move rotation point to center of image
+    g.TranslateTransform((float)b.Width / 2, (float)b.Height / 2);
+    //rotate
+    g.RotateTransform(angle);
+    //move image back
+    g.TranslateTransform(-(float)b.Width / 2, -(float)b.Height / 2);
+    //draw passed in image onto graphics object
+    g.DrawImage(b, new Point(0, 0));
+    return returnBitmap;
+}
+		
+		bool flag=false;
+	 async	void  Timer1Tick(object sender, EventArgs e)
+		{
+	 		Image img=Image.FromFile("小风车.png");
+			Bitmap bmp = new Bitmap(img);
+			if (flag) {
+				
+				for (int i = 0; i < 9; i++) {
+					notifyIcon1.Icon=ConvertToIcon(rotateImage(bmp,i*(-45)));
+					await Task.Delay(125);
+				}
+				
+			}
+		}
+		void AboutToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			MessageBox.Show("小风车 ver1.0\n作者：吃爆米花的小熊");
+		}
+		void DonateToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			Process.Start("https://partme.com/xiaoxiong");
+		}
+		//----------------------
+	}
+}
